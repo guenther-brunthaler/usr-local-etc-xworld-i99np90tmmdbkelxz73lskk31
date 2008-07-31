3b4320d241ed6e7a5726210aa02634aaac3c4d74
Guenther Brunthaler <root@xquad.xworld.mine.nu>
2008-07-31 13:02:52 +0200

    Added UMTS-GSM differentiation, and simplified Bash completion support

--- conf.d/local.stop-50disconnect_modem@xtreme.sh
+++ conf.d/local.stop-50disconnect_modem@xtreme.sh
@@ -7,18 +7,19 @@
 
 cleanup_l5urjd8tqdsx1ujh89hz02pxj() {
 	local PIDFILE PID
-	PIDFILE="/var/run/ppp-hui.pid"
-	if [ -f "$PIDFILE" ]; then
-		PID=`head -n1 "$PIDFILE"`
-		if [ -d "/proc/$PID" ]; then
-			ebegin "Disconnecting modem"
-			kill -HUP $PID
-			while [ -e "$PIDFILE" ]; do
-				sleep 1
-			done
-			eend $?
+	for PIDFILE in /var/run/ppp-hui-umts.pid /var/run/ppp-hui-gsm.pid; do
+		if [ -f "$PIDFILE" ]; then
+			PID=`head -n1 "$PIDFILE"`
+			if [ -d "/proc/$PID" ]; then
+				ebegin "Disconnecting modem"
+				kill -HUP $PID
+				while [ -e "$PIDFILE" ]; do
+					sleep 1
+				done
+				eend $?
+			fi
 		fi
-	fi
+	done
 }
 
 
