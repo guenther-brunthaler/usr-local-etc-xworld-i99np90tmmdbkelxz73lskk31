d4cc2e4ceae2c919b787d0ec9253a64a32502c8b
Guenther Brunthaler <root@xvaio.xworld.mine.nu>
2008-09-10 03:09:24 +0200

    Removed TrueCrypt Level 1 mount script
    Level1 is now integrated with "/"-filesystem.

--- conf.d/console_io.start-50tcmount@xvaio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/false
-# $HeadURL: /caches/xsvn/uxadm/trunk/usr/local/etc/conf.d/console_io.start-50tcmount@xvaio.sh $
-# $Author: root $
-# $Date: 2007-05-10T19:52:23.362749Z $
-# $Revision: 657 $
-
-/usr/local/sbin/xworld/bootprompt-launch --timeout 20 /usr/local/sbin/xworld/tcmount
