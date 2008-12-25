3ddf42b6688d1379345351da972652de01fc0eca
Guenther Brunthaler <root@xquad.xworld.mine.nu>
2008-12-25 16:06:07 +0100

    SQUID update plus some new files which were previously symlinks
    The new "manage" script will resolve symlinks in the live filesystem,
    therefore a couple of new files have been added.

--- overlay/etc/ppp/ip-up.d/89-ntpdate.sh
+++ overlay/etc/ppp/ip-up.d/89-ntpdate.sh
@@ -1,9 +1,35 @@
-#!/bin/sh
+#! /bin/false
 
-# Wait 10 seconds for the slow connection and set the system clock once
-sleep 10
-if [ -x /usr/sbin/ntpd ]; then
-	ntpd -q
-elif [ -x /usr/bin/ntpdate ]; then
-	ntpdate -s -b -u ntp2.inrim.it 3.it.pool.ntp.org 0.europe.pool.ntp.org
-fi
+
+(
+	log() {
+		local CATEGORY TOPIC
+		CATEGORY=$1; shift
+		TOPIC="`pwd`/$SCRIPT"
+		logger -p daemon.$CATEGORY -t "$TOPIC" "$*"
+	}
+
+
+	die() {
+		log err "$*"
+		false; exit
+	}
+
+
+	run() {
+		"$@" && return
+		die "Could not execute >>>$*<<<, return code ${?}"
+	}
+
+
+	# Wait 10 seconds for the slow connection to be established.
+	sleep 10
+	if test -x /usr/sbin/ntpd; then
+		run ntpd -q
+	elif test -x /usr/bin/ntpdate; then
+		run ntpdate -s -b -u \
+			ntp2.inrim.it 3.it.pool.ntp.org 0.europe.pool.ntp.org
+	fi
+	log notice "Setting CMOS clock to obtained NTP time"
+	run /etc/init.d/clock --quiet save
+) || exit
--- overlay/etc/ppp/ip-up.d/90-ntpd.sh
+++ overlay/etc/ppp/ip-up.d/90-ntpd.sh
@@ -1,9 +1,38 @@
-#!/bin/sh
+#! /bin/false
 
-# Wait 20 seconds for the slow connection and start the ntpd service
-sleep 20
-if [ -x /etc/init.d/ntpd ]; then
-	if ! /etc/init.d/ntpd --quiet status ; then
-		/etc/init.d/ntpd --quiet start
+
+(
+	log() {
+		local CATEGORY TOPIC
+		CATEGORY=$1; shift
+		TOPIC="`pwd`/$SCRIPT"
+		logger -p daemon.$CATEGORY -t "$TOPIC" "$*"
+	}
+
+
+	die() {
+		log err "$*"
+		false; exit
+	}
+
+
+	run() {
+		"$@" && return
+		die "Could not execute >>>$*<<<, return code ${?}"
+	}
+
+
+	# Wait 20 seconds for the slow connection and start the ntpd service.
+	sleep 20
+	if test -x /etc/init.d/ntpd; then
+		/etc/init.d/ntpd --quiet status || {
+			run test -f /etc/conf.d/ntpd
+			run . /etc/conf.d/ntpd
+			log debug "Starting NTP service daemon"
+			run start-stop-daemon --start \
+				--exec /usr/sbin/ntpd \
+				--pidfile /var/run/ntpd.pid \
+				-- -p /var/run/ntpd.pid ${NTPD_OPTS}
+		}
 	fi
-fi
+) || exit
