d7b46b0a12139a1768b26a45f3c07287c2a4f06c
Guenther Brunthaler <root@xquad.xworld.mine.nu>
2009-11-24 12:16:03 +0100

    Added squid proxy support to PPP startup/shutdown scripts

---
+++ ppp/ip-up.d/95-squid.sh
@@ -0,0 +1,13 @@
+#! /bin/false
+
+
+test x"${RC_GOT_FUNCTIONS}" != x"yes" && . /sbin/functions.sh
+(
+	SVCNAME=squid
+	if test -f /etc/init.d/"$SVCNAME"
+	then
+		test -f /etc/conf.d/"$SVCNAME" && . /etc/conf.d/"$SVCNAME"
+		. /etc/init.d/"$SVCNAME"
+		start
+	fi
+)
