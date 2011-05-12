12084a8f3adf7ea7ae98c17a56986dca931847d2
Guenther Brunthaler <root@xquad.xworld.mine.nu>
2011-05-12 15:13:49 +0200

    Required modifications for support of new baselayout-2

--- rc/runlevels.sh
+++ rc/runlevels.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-# $Header: /usr/local/etc/rc/RCS/runlevels.sh,v 1.2 2005/08/13 11:32:00 root Exp root $
+#! /bin/bash
 
 # Common functions
-[[ ${RC_GOT_FUNCTIONS} != "yes" ]] && source /sbin/functions.sh
+test -z "$RC_GOT_FUNCTIONS" \
+	&& . /usr/local/libexec/xworld/sh/gentoo-functions.sh
 
 einfo "Runlevels now: $(/usr/local/bin/runlevels -s)"
